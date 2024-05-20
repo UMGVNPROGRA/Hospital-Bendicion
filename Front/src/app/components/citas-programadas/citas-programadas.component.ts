9fffe4edf29ccc5182e002722dfc26be75cb74fe
josecarrera26 <jose.lusicarrera199526@outlook.com>
2024-05-20 17:50:15 -0600

    Cambios en MicroServicios citas y vistas citas, falta implementacion de consumo de apis.

---
+++ Front/src/app/components/citas-programadas/citas-programadas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-citas-programadas',
+  standalone: true,
+  imports: [],
+  templateUrl: './citas-programadas.component.html',
+  styleUrl: './citas-programadas.component.scss'
+})
+export class CitasProgramadasComponent {
+
+}
