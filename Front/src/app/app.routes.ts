9b7ce97ceb4a3b62cecfe5d81e072aeca5725dfe
josecarrera26 <jose.lusicarrera199526@outlook.com>
2024-05-20 10:22:08 -0600

    Citas creado

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import {CitasComponent} from "./components/citas/citas.component";
 
 export const routes: Routes = [
     {
@@ -7,6 +8,7 @@ export const routes: Routes = [
         loadComponent:()=>import('./components/layout/landing/landing.component'),
     },
     {path:'login', component:LoginComponent},
+    {path:'citas', component:CitasComponent},
 
     {path: '', redirectTo: 'index',pathMatch: 'full'},
     {path: '**',redirectTo: 'index',pathMatch: 'full'},
