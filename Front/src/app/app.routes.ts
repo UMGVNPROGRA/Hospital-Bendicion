6b386cd9c67b4ac10874901d5254155a70a38b81
Made <madissson27r@gmail.com>
2024-05-20 22:41:40 -0600

    On branch main  Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.    (use "git pull" to update your local branch)
     Changes to be committed:
    	modified:   Front/src/app/app.routes.ts
    	modified:   Front/src/app/components/receta/receta.component.html
    
     Changes not staged for commit:
    	modified:   Front/src/app/components/receta/receta.component.html

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import {CitasComponent} from "./components/citas/citas.component";
+import { RecetaComponent } from './components/receta/receta.component';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,7 @@ export const routes: Routes = [
     },
     {path:'login', component:LoginComponent},
     {path:'citas', component:CitasComponent},
-
+    {path:'recetas', component:RecetaComponent},
     {path: '', redirectTo: 'index',pathMatch: 'full'},
     {path: '**',redirectTo: 'index',pathMatch: 'full'},
 ];
