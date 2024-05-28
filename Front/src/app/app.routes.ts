b663dab16e10109caf2a1ef78f90031f596e953d
FernandoChial <pchialv@miumg.edu.gt>
2024-05-27 21:44:19 -0600

    cambios menu y usuarios

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -5,14 +5,17 @@ import { FacturaComponent } from './components/factura/factura.component';
 import { RecetaComponent } from './components/receta/receta.component';
 import { PacienteComponent } from './components/paciente/paciente.component';
 import {CitasProgramadasComponent} from "./components/citas-programadas/citas-programadas.component";
+
 import { Component } from '@angular/core';
 import { HistorialComponent } from './components/historial/historial.component';
 import { MedicoComponent } from './components/medico/medico.component';
 import { MenuAdministradorComponent } from './components/menu-principal/menu-administrador/menu-administrador.component';
 import { MenuSecretariaComponent } from './components/menu-principal/menu-secretaria/menu-secretaria.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { ModulosadminComponent } from './components/menu-principal/modulos-admin/modulosadmin.component';
 
 export const routes: Routes = [
+
     {
         path:'index',
         loadComponent:()=>import('./components/layout/landing/landing.component'),
@@ -22,16 +25,27 @@ export const routes: Routes = [
     {path:'usuarios', component:UsuarioComponent},
     {path:'paciente', component:PacienteComponent},
 
-    {path:'recetas', component:RecetaComponent},
-    {path:'citasprogramadas', component:CitasProgramadasComponent},
+  { path: 'recetas', component: RecetaComponent },
+  { path: 'citasprogramadas', component: CitasProgramadasComponent },
 
-    {path:'historial', component:HistorialComponent},
+  { path: 'historial', component: HistorialComponent },
 
     {path:'medico',component:MedicoComponent},
     {path:'administrador',component:MenuAdministradorComponent},
     {path:'secretaria',component:MenuSecretariaComponent},
     {path:'factura', component:FacturaComponent},
 
-    {path: '', redirectTo: 'index',pathMatch: 'full'},
-    {path: '**',redirectTo: 'index',pathMatch: 'full'},
+  {
+    path: 'administrador', component:MenuAdministradorComponent ,
+    children: [
+      //{ path: "modadmin", component: MenuAdministradorComponent },
+      { path: "user", component: UsuarioComponent },
+      { path: 'modhistorial', component: HistorialComponent },
+
+    ]
+  },
+  { path: 'secretaria', component: MenuSecretariaComponent },
+
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: '**', redirectTo: 'index', pathMatch: 'full' },
 ];
