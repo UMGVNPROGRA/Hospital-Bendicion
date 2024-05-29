7659e8a3d839ddef08257fada6271ee821446b2b
Agencias-Way <way_odoo@agenciasway.com>
2024-05-28 19:30:52 -0600

    vista_factura_cita

--- Front/src/app/components/factura/factura.component.ts
+++ Front/src/app/components/factura/factura.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'app/servicios/api.service';
+import { CitasI } from '../modelos/citas.interface';
+import { CommonModule, NgFor } from '@angular/common';
+import { NgModel } from '@angular/forms';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-factura',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor,FormsModule],
   templateUrl: './factura.component.html',
-  styleUrl: './factura.component.scss'
+  styleUrl: './factura.component.scss',
 })
-export class FacturaComponent {
+export class FacturaComponent implements OnInit {
+  citas: CitasI[] | undefined;
+  filaSeleccionado: any;
+  nombre_paciente:any;
+  nombre_medico:any;
 
+  constructor(
+    private activerouter: ActivatedRoute,
+    private router: Router,
+    private api: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.cargaCitas();
+    this.nombre_paciente="";
+    this.nombre_medico="";
+  }
+
+  mostrarDatos() {
+   
+    this.nombre_paciente=this.filaSeleccionado.paciente.nombre;
+    this.nombre_medico=this.filaSeleccionado.id_medico.nombre_medico;
+  }
+
+  cargaCitas() {
+    this.api.ListadoCitas().subscribe((data) => {
+      //console.log(data);
+      this.citas = data;
+      console.log(this.citas);
+    });
+  }
 }
