195d419c4bea77305494c44ab13c361e802f30b6
MadelinRobles <128769139+MadelinRobles@users.noreply.github.com>
2024-05-28 21:58:49 -0600

    Changes to be committed: 	modified:   Front/src/app/components/medicamentos/medicamentos.component.html 	modified:   Front/src/app/components/medicamentos/medicamentos.component.ts

--- Front/src/app/components/medicamentos/medicamentos.component.ts
+++ Front/src/app/components/medicamentos/medicamentos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RecetaComponent } from '../receta/receta.component';
+import { CitasComponent } from '../citas/citas.component';
 
 @Component({
   selector: 'app-medicamentos',
   standalone: true,
-  imports: [],
+  imports: [RecetaComponent,CitasComponent],
   templateUrl: './medicamentos.component.html',
   styleUrl: './medicamentos.component.scss'
 })
