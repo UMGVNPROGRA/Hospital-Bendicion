888a40b18339e36e1a07a822a7ae7f65cf2b795f
MadelinRobles2 <madelin.robles@agenciasway.com>
2024-05-30 16:27:00 -0600

    Changes to be committed: 	modified:   Front/src/app/components/receta/receta.component.html 	modified:   Front/src/app/components/receta/receta.component.ts 	modified:   Front/src/app/interfaces/medicamentos-interface.ts 	modified:   Front/src/app/services/receta.service.ts

--- Front/src/app/components/receta/receta.component.ts
+++ Front/src/app/components/receta/receta.component.ts
@@ -3,51 +3,98 @@ import { LoginService } from 'app/services/login.service';
 import { CitasI } from '../modelos/citas.interface';
 import { RecetaService } from 'app/services/receta.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CitasInterfaces } from 'app/interfaces/citas-interfaces';
+import { Medicamentos, MedicamentosReceta } from 'app/interfaces/medicamentos-interface';
 @Component({
   selector: 'app-receta',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './receta.component.html',
   styleUrl: './receta.component.scss'
 })
 export class RecetaComponent implements OnInit {
   citas: CitasInterfaces | undefined;
-  idMed  : number = 0;
+  idCita: number = 0;
+
+  medicamento: MedicamentosReceta | undefined;
+  idMed: string = "";
+  descripcion: string = "";
+
+  arrayMed: MedicamentosReceta[] = []
+  recetaForm!: FormGroup;
+
+  agregarMed() {
+    if (this.medicamento) {
+      console.log(this.medicamento.id_medicamento)
+
+      this.agregarDetalle(this.medicamento.id_medicamento, this.medicamento.nombre_medicamento, this.descripcion)
+    }
+
+  }
+
   private recetasSrv = inject(RecetaService);
+  private fb = inject(FormBuilder);
 
   constructor(private serviceLogin: LoginService) {
 
   }
 
+
+
   ngOnInit() {
     //this.cargaCitas(1);
+    this.recetaForm = this.fb.group({
+      idCit: ['',Validators.required],
+
+      detalle: ['', Validators.required],
+    });
+    this.recetaForm.patchValue({
+      detalle: this.arrayMed
+    });
   }
 
-tipoConsulta(){
-  if(this.serviceLogin.userRoleValue==='administrador'){
-    
+  agregarDetalle(id: number, nom: String, des: string) {
+    if (this.medicamento) {
+      this.medicamento.descripcionRecta = this.descripcion
+      this.arrayMed.push(this.medicamento);
+    }
+    console.log("DATOS", this.recetaForm.value);
+
   }
 
-}
 
-cargaCitas(id:number) {
-  this.recetasSrv.getcitasById(id).subscribe((data) => {
+  cargaCitas(id: number) {
+    this.recetasSrv.getcitasById(id).subscribe((data) => {
 
-    console.log(id);
-    this.citas = data;
-    console.log(this.citas);
-  });
-  };
-
-  cargaMedi() {
-    this.recetasSrv.getcitasMedicamentoByCod("ASP","A").subscribe((data) => {
-      //console.log(data);
+      console.log(id);
       this.citas = data;
       console.log(this.citas);
     });
-    };
+  };
+
+  cargaMedi(cod: string) {
+    this.recetasSrv.getcitasMedicamentoByCod(cod, "A").subscribe((data) => {
+      //console.log(data);
+      this.medicamento = data;
+      console.log(this.medicamento);
+    });
+  };
+
+  finalizar(){
+   let pr = this.recetaForm.get('idCit')?.setValue(this.citas?.id_cita)
+    console.log(this.recetaForm.value)
+    
+    this.recetasSrv.saveReceta(this.recetaForm.value).subscribe({
+      next: response => {
+        console.log('Receta Guardada', response);
+      },
+      error: error => {
+        console.error('Error al guardar Receta', error);
+      }
+    });
+  }
+
 }
 
 
--- Front/src/app/interfaces/medicamentos-interface.ts
+++ Front/src/app/interfaces/medicamentos-interface.ts
@@ -6,3 +6,13 @@ export interface Medicamentos {
     "estado": String
     "tipo_medicamento": String
 }
+
+export interface MedicamentosReceta {
+    "id_medicamento": number
+    "codigo_medicamento": String
+    "nombre_medicamento": String
+    "tipo_medida": String
+    "estado": String
+    "tipo_medicamento": String
+    "descripcionRecta": String
+}
